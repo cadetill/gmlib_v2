087be3ded9e5c06f329fda7c4d0cfd500b683a71
cadetill <cadetill@gmail.com>
2022-08-11 19:07:57 +0200

    .

--- src/GMLib.Sets.pas
+++ src/GMLib.Sets.pas
@@ -2,7 +2,7 @@
   @abstract(Sets for GMLib.)
   @author(Xavier Martinez (cadetill) <cadetill@gmail.com>)
   @created(August 2, 2022)
-  @lastmod(August 2, 2022)
+  @lastmod(August 11, 2022)
 
   The GMLib.Sets unit contains all sets used by GMLib.
 }
@@ -20,6 +20,56 @@ type
              lnEnglish,
              // French exception language
              lnFrench);
+
+  // @include(..\Help\docs\GMLib.Sets.TGMAPIVer.txt)
+  TGMAPIVer = (av347, av348, av349, avWeekly, avQuarterly, avBeta);
+
+  // @include(..\Help\docs\GMLib.Sets.TGMAPILang.txt)
+  TGMAPILang = (lAfrikaans, lAlbanian, lAmharic, lArabic, lArmenian, lAzerbaijani, lBasque, lBelarusian,
+                lBengali, lBosnian, lBulgarian, lBurmese, lCatalan, lChinese, lChinese_Simp, lChinese_HK,
+                lChinese_Trad, lCroatian, lCzech, lDanish, lDutch, lEnglish, lEnglish_Aust, lEnglish_GB,
+                lEstonian, lFarsi, lFinnish, lFilipino, lFrench, lFrench_Ca, lGalician, lGeorgian, lGerman,
+                lGreek, lGujarati, lHebrew, lHindi, lHungarian, lIcelandic, lIndonesian, lItalian, lJapanese,
+                lKannada, lKazakh, lKhmer, lKorean, lKyrgyz, lLao, lLatvian, lLithuanian, lMacedonian,
+                lMalay, lMalayalam, lMarathi, lMongolian, lNepali, lNorwegian, lPolish, lPortuguese,
+                lPortuguese_Br, lPortuguese_Ptg, lPunjabi, lRomanian, lRussian, lSerbian, lSinhalese,
+                lSlovak, lSlovenian, lSpanish, lSpanish_LA, lSwahili, lSwedish, lTamil, lTelugu, lThai,
+                lTurkish, lUkrainian, lUrdu, lUzbek, lVietnamese, lZulu, lUndefined);
+
+  // @include(..\Help\docs\GMLib.Sets.TGMAPIRegion.txt)
+  TGMAPIRegion = (rAfghanistan, rAlbania, rAlgeria, rAmerican_Samoa, rAndorra, rAngola, rAnguilla, rAntarctica,
+                  rAntigua_Barbuda, rArgentina, rArmenia, rAruba, rAscension_Island, rAustralia, rAustriam,
+                  rAzerbaijan, rBahamas, rBahrain, rBangladesh, rBarbados, rBelarus, rBelgium, rBelize, rBenin,
+                  rBermuda, rBhutan, rBolivia, rBosnia_Herzegovina, rBotswana, rBouvet_Island, rBrazil,
+                  rBritish_Indian_OT, rBritish_Virgin_Is, rBrunei, rBulgaria, rBurkina_Faso, rBurundi, rCambodia,
+                  rCameroon, rCanada, rCanary_Islands, rCape_Verde, rCaribbean_Netherlands, rCayman_Islands,
+                  rCentral_African_Rep, rCeuta_Melilla, rChad, rChile, rChina, rChristmas_Island, rClipperton_Island,
+                  rCocos_Islands, rColombia, rComoros, rCongo_Brazzaville, rCongo_Kinshasa, rCook_Islands,
+                  rCosta_Rica, rCroatia, rCuba, rCuracao, rCyprus, rCzechia, rCote_Ivoire, rDenmark, rDiego_Garcia,
+                  rDjibouti, rDominica, rDominican_Republic, rEcuador, rEgypt, rEl_Salvador, rEquatorial_Guinea,
+                  rEritrea, rEstonia, rEswatini, rEthiopia, rFalkland_Islands, rFaroe_Islands, rFiji, rFinland,
+                  rFrance, rFrench_Guiana, rFrench_Polynesia, rFrench_Southern_Territories, rGabon, rGambia, rGeorgia,
+                  rGermany, rGhana, rGibraltar, rGreece, rGreenland, rGrenada, rGuadeloupe, rGuam, rGuatemala,
+                  rGuernsey, rGuinea, rGuinea_Bissau, rGuyana, rHaiti, rHeard_McDonald, rHonduras, rHong_Kong,
+                  rHungary, rIceland, rIndia, rIndonesia, rIran, rIraq, rIreland, rIsle_Man, rIsrael, rItaly, rJamaica,
+                  rJapan, rJersey, rJordan, rKazakhstan, rKenya, rKiribati, rKosovo, rKuwait, rKyrgyzstan, rLaos,
+                  rLatvia, rLebanon, rLesotho, rLiberia, rLibya, rLiechtenstein, rLithuania, rLuxembourg, rMacao,
+                  rMadagascar, rMalawi, rMalaysia, rMaldives, rMali, rMalta, rMarshall_Islands, rMartinique,
+                  rMauritania, rMauritius, rMayotte, rMexico, rMicronesia, rMoldova, rMonaco, rMongolia, rMontenegro,
+                  rMontserrat, rMorocco, rMozambique, rMyanmar, rNamibia, rNauru, rNepal, rNetherlands, rNew_Caledonia,
+                  rNew_Zealand, rNicaragua, rNiger, rNigeria, rNiue, rNorfolk_Island, rNorth_Korea, rNorth_Macedonia,
+                  rNorthern_Mariana, rNorway, rOman, rPakistan, rPalau, rPalestine, rPanama, rPapua_New_Guinea,
+                  rParaguay, rPeru, rPhilippines, rPitcairn_Islands, rPoland, rPortugal, rPuerto_Rico, rQatar,
+                  rRomania, rRussia, rRwanda, rReunion, rSamoa, rSan_Marino, rSaudi_Arabia, rSenegal, rSerbia,
+                  rSeychelles, rSierra_Leone, rSingapore, rSint_Maarten, rSlovakia, rSlovenia, rSolomon_Islands,
+                  rSomalia, rSouth_Africa, rSouth_Georgia_South_Sandwich, rSouth_Korea, rSouth_Sudan, rSpain,
+                  rSri_Lanka, rSt_Barthelemy, rSt_Helena, rSt_Kitts_Nevis, rSt_Lucia, rSt_Martin, rSt_Pierre_Miquelon,
+                  rSt_Vincent_Grenadines, rSudan, rSuriname, rSvalbard_Jan_Mayen, rSweden, rSwitzerland, rSyria,
+                  rSao_Tome_Principe, rTaiwan, rTajikistan, rTanzania, rThailand, rTimor_Leste, rTogo, rTokelau, rTonga,
+                  rTrinidad_Tobago, rTristan_da_Cunha, rTunisia, rTurkey, rTurkmenistan, rTurks_Caicos_Islands, rTuvalu,
+                  rUS_Outlying_Islands, rUS_Virgin_Islands, rUganda, rUkraine, rUnited_Arab_Emirates, rUnited_Kingdom,
+                  rUnited_States, rUruguay, rUzbekistan, rVanuatu, rVatican_City, rVenezuela, rVietnam, rWallis_Futuna,
+                  rWestern_Sahara, rYemen, rZambia, rZimbabwe, rAland_Islands, rUndefined);
 
   // @include(..\Help\docs\GMLib.Sets.TGMControlPosition.txt)
   TGMControlPosition = (cpBOTTOM_CENTER, cpBOTTOM_LEFT, cpBOTTOM_RIGHT,
--- src/GMLib.Transform.pas
+++ src/GMLib.Transform.pas
@@ -2,7 +2,7 @@
   @abstract(Transform a data from Delphi to JS ans JS to Delphi.)
   @author(Xavier Martinez (cadetill) <cadetill@gmail.com>)
   @created(August 2, 2022)
-  @lastmod(August 2, 2022)
+  @lastmod(August 11, 2022)
 
   The GMLib.Transform unit contains all functions needed to transform a JavaScript value to Delphi value and viceversa.
 }
@@ -19,6 +19,13 @@ type
   public
     // @include(..\Help\docs\GMLib.Transform.TGMTransform.GMBoolToStr.txt)
     class function GMBoolToStr(Value: Boolean; UseBoolStrs: Boolean = False): string; static;
+
+    // @include(..\Help\docs\GMLib.Transform.TGMTransform.APIVerToStr.txt)
+    class function APIVerToStr(Value: TGMAPIVer): string; static;
+    // @include(..\Help\docs\GMLib.Transform.TGMTransform.APILangToStr.txt)
+    class function APILangToStr(Value: TGMAPILang): string; static;
+    // @include(..\Help\docs\GMLib.Transform.TGMTransform.APIRegionToStr.txt)
+    class function APIRegionToStr(Value: TGMAPIRegion): string; static;
 
     // @include(..\Help\docs\GMLib.Transform.TGMTransform.PositionToStr.txt)
     class function PositionToStr(Value: TGMControlPosition): string; static;
@@ -46,8 +53,6 @@ type
 
 (*
 
-    // @include(..\Help\docs\GMLib.Transform.TGMTransform.APILangToStr.txt)
-    class function APILangToStr(APILang: TGMAPILang): string; static;
     // @include(..\Help\docs\GMLib.Transform.TGMTransform.VisibilityToStr.txt)
     class function VisibilityToStr(Visibility: TGMVisibility): string; static;
     // @include(..\Help\docs\GMLib.Transform.TGMTransform.StrToVisibility.txt)
@@ -73,6 +78,369 @@ uses
 
 
 { TGMTransform }
+
+class function TGMTransform.APILangToStr(Value: TGMAPILang): string;
+begin
+  case Value of
+    lAfrikaans: Result := 'af';
+    lAlbanian: Result := 'sq';
+    lAmharic: Result := 'am';
+    lArabic: Result := 'ar';
+    lArmenian: Result := 'hy';
+    lAzerbaijani: Result := 'az';
+    lBasque: Result := 'eu';
+    lBelarusian: Result := 'be';
+    lBengali: Result := 'bn';
+    lBosnian: Result := 'bs';
+    lBulgarian: Result := 'bg';
+    lBurmese: Result := 'my';
+    lCatalan: Result := 'ca';
+    lChinese: Result := 'zh';
+    lChinese_Simp: Result := 'zh-CN';
+    lChinese_HK: Result := 'zh-HK';
+    lChinese_Trad: Result := 'zh-TW';
+    lCroatian: Result := 'hr';
+    lCzech: Result := 'cs';
+    lDanish: Result := 'da';
+    lDutch: Result := 'nl';
+    lEnglish: Result := 'en';
+    lEnglish_Aust: Result := 'en-AU';
+    lEnglish_GB: Result := 'en-GB';
+    lEstonian: Result := 'et';
+    lFarsi: Result := 'fa';
+    lFinnish: Result := 'fi';
+    lFilipino: Result := 'fil';
+    lFrench: Result := 'fr';
+    lFrench_Ca: Result := 'fr-CA';
+    lGalician: Result := 'gl';
+    lGeorgian: Result := 'ka';
+    lGerman: Result := 'de';
+    lGreek: Result := 'el';
+    lGujarati: Result := 'gu';
+    lHebrew: Result := 'iw';
+    lHindi: Result := 'hi';
+    lHungarian: Result := 'hu';
+    lIcelandic: Result := 'is';
+    lIndonesian: Result := 'id';
+    lItalian: Result := 'it';
+    lJapanese: Result := 'ja';
+    lKannada: Result := 'kn';
+    lKazakh: Result := 'kk';
+    lKhmer: Result := 'km';
+    lKorean: Result := 'ko';
+    lKyrgyz: Result := 'ky';
+    lLao: Result := 'lo';
+    lLatvian: Result := 'lv';
+    lLithuanian: Result := 'lt';
+    lMacedonian: Result := 'mk';
+    lMalay: Result := 'ms';
+    lMalayalam: Result := 'ml';
+    lMarathi: Result := 'mr';
+    lMongolian: Result := 'mn';
+    lNepali: Result := 'ne';
+    lNorwegian: Result := 'no';
+    lPolish: Result := 'pl';
+    lPortuguese: Result := 'pt';
+    lPortuguese_Br: Result := 'pt-BR';
+    lPortuguese_Ptg: Result := 'pt-PT';
+    lPunjabi: Result := 'pa';
+    lRomanian: Result := 'ro';
+    lRussian: Result := 'ru';
+    lSerbian: Result := 'sr';
+    lSinhalese: Result := 'si';
+    lSlovak: Result := 'sk';
+    lSlovenian: Result := 'sl';
+    lSpanish: Result := 'es';
+    lSpanish_LA: Result := 'es-419';
+    lSwahili: Result := 'sw';
+    lSwedish: Result := 'sv';
+    lTamil: Result := 'ta';
+    lTelugu: Result := 'te';
+    lThai: Result := 'th';
+    lTurkish: Result := 'tr';
+    lUkrainian: Result := 'uk';
+    lUrdu: Result := 'ur';
+    lUzbek: Result := 'uz';
+    lVietnamese: Result := 'vi';
+    lZulu: Result := 'zu';
+    lUndefined: Result := 'en';
+  end;
+end;
+
+class function TGMTransform.APIRegionToStr(Value: TGMAPIRegion): string;
+begin
+  case Value of
+    rAfghanistan: Result := 'AF';
+    rAlbania: Result := 'AL';
+    rAlgeria: Result := 'DZ';
+    rAmerican_Samoa: Result := 'AS';
+    rAndorra: Result := 'AD';
+    rAngola: Result := 'AO';
+    rAnguilla: Result := 'AI';
+    rAntarctica: Result := 'AQ';
+    rAntigua_Barbuda: Result := 'AG';
+    rArgentina: Result := 'AR';
+    rArmenia: Result := 'AM';
+    rAruba: Result := 'AW';
+    rAscension_Island: Result := 'AC';
+    rAustralia: Result := 'AU';
+    rAustriam: Result := 'AT';
+    rAzerbaijan: Result := 'AZ';
+    rBahamas: Result := 'BS';
+    rBahrain: Result := 'BH';
+    rBangladesh: Result := 'BD';
+    rBarbados: Result := 'BB';
+    rBelarus: Result := 'BY';
+    rBelgium: Result := 'BE';
+    rBelize: Result := 'BZ';
+    rBenin: Result := 'BJ';
+    rBermuda: Result := 'BM';
+    rBhutan: Result := 'BT';
+    rBolivia: Result := 'BO';
+    rBosnia_Herzegovina: Result := 'BA';
+    rBotswana: Result := 'BW';
+    rBouvet_Island: Result := 'BV';
+    rBrazil: Result := 'BR';
+    rBritish_Indian_OT: Result := 'IO';
+    rBritish_Virgin_Is: Result := 'VG';
+    rBrunei: Result := 'BN';
+    rBulgaria: Result := 'BG';
+    rBurkina_Faso: Result := 'BF';
+    rBurundi: Result := 'BI';
+    rCambodia: Result := 'KH';
+    rCameroon: Result := 'CM';
+    rCanada: Result := 'CA';
+    rCanary_Islands: Result := 'IC';
+    rCape_Verde: Result := 'CV';
+    rCaribbean_Netherlands: Result := 'BQ';
+    rCayman_Islands: Result := 'KY';
+    rCentral_African_Rep: Result := 'CF';
+    rCeuta_Melilla: Result := 'EA';
+    rChad: Result := 'TD';
+    rChile: Result := 'CL';
+    rChina: Result := 'CN';
+    rChristmas_Island: Result := 'CX';
+    rClipperton_Island: Result := 'CP';
+    rCocos_Islands: Result := 'CC';
+    rColombia: Result := 'CO';
+    rComoros: Result := 'KM';
+    rCongo_Brazzaville: Result := 'CG';
+    rCongo_Kinshasa: Result := 'CD';
+    rCook_Islands: Result := 'CK';
+    rCosta_Rica: Result := 'CR';
+    rCroatia: Result := 'HR';
+    rCuba: Result := 'CU';
+    rCuracao: Result := 'CW';
+    rCyprus: Result := 'CY';
+    rCzechia: Result := 'CZ';
+    rCote_Ivoire: Result := 'CI';
+    rDenmark: Result := 'DK';
+    rDiego_Garcia: Result := 'DG';
+    rDjibouti: Result := 'DJ';
+    rDominica: Result := 'DM';
+    rDominican_Republic: Result := 'DO';
+    rEcuador: Result := 'EC';
+    rEgypt: Result := 'EG';
+    rEl_Salvador: Result := 'SV';
+    rEquatorial_Guinea: Result := 'GQ';
+    rEritrea: Result := 'ER';
+    rEstonia: Result := 'EE';
+    rEswatini: Result := 'SZ';
+    rEthiopia: Result := 'ET';
+    rFalkland_Islands: Result := 'FK';
+    rFaroe_Islands: Result := 'FO';
+    rFiji: Result := 'FJ';
+    rFinland: Result := 'FI';
+    rFrance: Result := 'FR';
+    rFrench_Guiana: Result := 'GF';
+    rFrench_Polynesia: Result := 'PF';
+    rFrench_Southern_Territories: Result := 'TF';
+    rGabon: Result := 'GA';
+    rGambia: Result := 'GM';
+    rGeorgia: Result := 'GE';
+    rGermany: Result := 'DE';
+    rGhana: Result := 'GH';
+    rGibraltar: Result := 'GI';
+    rGreece: Result := 'GR';
+    rGreenland: Result := 'GL';
+    rGrenada: Result := 'GD';
+    rGuadeloupe: Result := 'GP';
+    rGuam: Result := 'GU';
+    rGuatemala: Result := 'GT';
+    rGuernsey: Result := 'GG';
+    rGuinea: Result := 'GN';
+    rGuinea_Bissau: Result := 'GW';
+    rGuyana: Result := 'GY';
+    rHaiti: Result := 'HT';
+    rHeard_McDonald: Result := 'HM';
+    rHonduras: Result := 'HN';
+    rHong_Kong: Result := 'HK';
+    rHungary: Result := 'HU';
+    rIceland: Result := 'IS';
+    rIndia: Result := 'IN';
+    rIndonesia: Result := 'ID';
+    rIran: Result := 'IR';
+    rIraq: Result := 'IQ';
+    rIreland: Result := 'IE';
+    rIsle_Man: Result := 'IM';
+    rIsrael: Result := 'IL';
+    rItaly: Result := 'IT';
+    rJamaica: Result := 'JM';
+    rJapan: Result := 'JP';
+    rJersey: Result := 'JE';
+    rJordan: Result := 'JO';
+    rKazakhstan: Result := 'KZ';
+    rKenya: Result := 'KE';
+    rKiribati: Result := 'KI';
+    rKosovo: Result := 'XK';
+    rKuwait: Result := 'KW';
+    rKyrgyzstan: Result := 'KG';
+    rLaos: Result := 'LA';
+    rLatvia: Result := 'LV';
+    rLebanon: Result := 'LB';
+    rLesotho: Result := 'LS';
+    rLiberia: Result := 'LR';
+    rLibya: Result := 'LY';
+    rLiechtenstein: Result := 'LI';
+    rLithuania: Result := 'LT';
+    rLuxembourg: Result := 'LU';
+    rMacao: Result := 'MO';
+    rMadagascar: Result := 'MG';
+    rMalawi: Result := 'MW';
+    rMalaysia: Result := 'MY';
+    rMaldives: Result := 'MV';
+    rMali: Result := 'ML';
+    rMalta: Result := 'MT';
+    rMarshall_Islands: Result := 'MH';
+    rMartinique: Result := 'MQ';
+    rMauritania: Result := 'MR';
+    rMauritius: Result := 'MU';
+    rMayotte: Result := 'YT';
+    rMexico: Result := 'MX';
+    rMicronesia: Result := 'FM';
+    rMoldova: Result := 'MD';
+    rMonaco: Result := 'MC';
+    rMongolia: Result := 'MN';
+    rMontenegro: Result := 'ME';
+    rMontserrat: Result := 'MS';
+    rMorocco: Result := 'MA';
+    rMozambique: Result := 'MZ';
+    rMyanmar: Result := 'MM';
+    rNamibia: Result := 'NA';
+    rNauru: Result := 'NR';
+    rNepal: Result := 'NP';
+    rNetherlands: Result := 'NL';
+    rNew_Caledonia: Result := 'NC';
+    rNew_Zealand: Result := 'NZ';
+    rNicaragua: Result := 'NI';
+    rNiger: Result := 'NE';
+    rNigeria: Result := 'NG';
+    rNiue: Result := 'NU';
+    rNorfolk_Island: Result := 'NF';
+    rNorth_Korea: Result := 'KP';
+    rNorth_Macedonia: Result := 'MK';
+    rNorthern_Mariana: Result := 'MP';
+    rNorway: Result := 'NO';
+    rOman: Result := 'OM';
+    rPakistan: Result := 'PK';
+    rPalau: Result := 'PW';
+    rPalestine: Result := 'PS';
+    rPanama: Result := 'PA';
+    rPapua_New_Guinea: Result := 'PG';
+    rParaguay: Result := 'PY';
+    rPeru: Result := 'PE';
+    rPhilippines: Result := 'PH';
+    rPitcairn_Islands: Result := 'PN';
+    rPoland: Result := 'PL';
+    rPortugal: Result := 'PT';
+    rPuerto_Rico: Result := 'PR';
+    rQatar: Result := 'QA';
+    rRomania: Result := 'RO';
+    rRussia: Result := 'RU';
+    rRwanda: Result := 'RW';
+    rReunion: Result := 'RE';
+    rSamoa: Result := 'WS';
+    rSan_Marino: Result := 'SM';
+    rSaudi_Arabia: Result := 'SA';
+    rSenegal: Result := 'SN';
+    rSerbia: Result := 'RS';
+    rSeychelles: Result := 'SC';
+    rSierra_Leone: Result := 'SL';
+    rSingapore: Result := 'SG';
+    rSint_Maarten: Result := 'SX';
+    rSlovakia: Result := 'SK';
+    rSlovenia: Result := 'SI';
+    rSolomon_Islands: Result := 'SB';
+    rSomalia: Result := 'SO';
+    rSouth_Africa: Result := 'ZA';
+    rSouth_Georgia_South_Sandwich: Result := 'GS';
+    rSouth_Korea: Result := 'KR';
+    rSouth_Sudan: Result := 'SS';
+    rSpain: Result := 'ES';
+    rSri_Lanka: Result := 'LK';
+    rSt_Barthelemy: Result := 'BL';
+    rSt_Helena: Result := 'SH';
+    rSt_Kitts_Nevis: Result := 'KN';
+    rSt_Lucia: Result := 'LC';
+    rSt_Martin: Result := 'MF';
+    rSt_Pierre_Miquelon: Result := 'PM';
+    rSt_Vincent_Grenadines: Result := 'VC';
+    rSudan: Result := 'SD';
+    rSuriname: Result := 'SR';
+    rSvalbard_Jan_Mayen: Result := 'SJ';
+    rSweden: Result := 'SE';
+    rSwitzerland: Result := 'CH';
+    rSyria: Result := 'SY';
+    rSao_Tome_Principe: Result := 'ST';
+    rTaiwan: Result := 'TW';
+    rTajikistan: Result := 'TJ';
+    rTanzania: Result := 'TZ';
+    rThailand: Result := 'TH';
+    rTimor_Leste: Result := 'TL';
+    rTogo: Result := 'TG';
+    rTokelau: Result := 'TK';
+    rTonga: Result := 'TO';
+    rTrinidad_Tobago: Result := 'TT';
+    rTristan_da_Cunha: Result := 'TA';
+    rTunisia: Result := 'TN';
+    rTurkey: Result := 'TR';
+    rTurkmenistan: Result := 'TM';
+    rTurks_Caicos_Islands: Result := 'TC';
+    rTuvalu: Result := 'TV';
+    rUS_Outlying_Islands: Result := 'UM';
+    rUS_Virgin_Islands: Result := 'VI';
+    rUganda: Result := 'UG';
+    rUkraine: Result := 'UA';
+    rUnited_Arab_Emirates: Result := 'AE';
+    rUnited_Kingdom: Result := 'GB';
+    rUnited_States: Result := 'US';
+    rUruguay: Result := 'UY';
+    rUzbekistan: Result := 'UZ';
+    rVanuatu: Result := 'VU';
+    rVatican_City: Result := 'VA';
+    rVenezuela: Result := 'VE';
+    rVietnam: Result := 'VN';
+    rWallis_Futuna: Result := 'WF';
+    rWestern_Sahara: Result := 'EH';
+    rYemen: Result := 'YE';
+    rZambia: Result := 'ZM';
+    rZimbabwe: Result := 'ZW';
+    rAland_Islands: Result := 'AX';
+    rUndefined: Result := 'ES';
+  end;
+end;
+
+class function TGMTransform.APIVerToStr(Value: TGMAPIVer): string;
+begin
+  case Value of
+    av347: Result := '3.47';
+    av348: Result := '3.48';
+    av349: Result := '3.49';
+    avWeekly: Result := 'weekly';
+    avQuarterly: Result := 'quarterly';
+    avBeta: Result := 'beta';
+  end;
+end;
 
 class function TGMTransform.GestureHandlingToStr(Value: TGMGestureHandling): string;
 begin
