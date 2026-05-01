fb6b5a302897580cb9662711524cc1c11133d4cd
cadetill <cadetill@gmail.com>
2026-05-01 16:37:35 +0200

    v2

--- src/Fmx/uGMLib.Fmx.Register.pas
+++ src/Fmx/uGMLib.Fmx.Register.pas
@@ -19,12 +19,41 @@ implementation
 
 uses
   System.Classes,
+  Winapi.Windows,
   DesignIntf,
+  ToolsAPI,
   uGMLib.Fmx.Map;
+
+procedure AddBitmapToSplashScreen;
+var
+  SplashBitmap: HBITMAP;
+  TempVersion : string;
+begin
+  if not Assigned(SplashScreenServices) then
+    Exit;
+
+  SplashBitmap := LoadBitmap(FindResourceHInstance(HInstance), 'GMLibSplashFmx');
+  if SplashBitmap = 0 then
+    Exit;
+
+  TempVersion := '2.0.0.3';
+
+  try
+    SplashScreenServices.AddPluginBitmap(
+      'GMLib VCL ' + TempVersion,
+      SplashBitmap,
+      False,
+      'Google Maps JavaScript API wrapper'
+    );
+  finally
+    DeleteObject(SplashBitmap);
+  end;
+end;
 
 procedure Register;
 begin
   RegisterComponents('GMLib', [TGMLibFmxMap]);
+  AddBitmapToSplashScreen;
 end;
 
 end.
--- src/Vcl/uGMLib.Vcl.Register.pas
+++ src/Vcl/uGMLib.Vcl.Register.pas
@@ -19,12 +19,41 @@ implementation
 
 uses
   System.Classes,
+  Winapi.Windows,
   DesignIntf,
+  ToolsAPI,
   uGMLib.Vcl.Map;
+
+procedure AddBitmapToSplashScreen;
+var
+  SplashBitmap: HBITMAP;
+  TempVersion : string;
+begin
+  if not Assigned(SplashScreenServices) then
+    Exit;
+
+  SplashBitmap := LoadBitmap(FindResourceHInstance(HInstance), 'GMLibSplashVcl');
+  if SplashBitmap = 0 then
+    Exit;
+
+  TempVersion := '2.0.0.3';
+
+  try
+    SplashScreenServices.AddPluginBitmap(
+      'GMLib VCL ' + TempVersion,
+      SplashBitmap,
+      False,
+      'Google Maps JavaScript API wrapper'
+    );
+  finally
+    DeleteObject(SplashBitmap);
+  end;
+end;
 
 procedure Register;
 begin
   RegisterComponents('GMLib', [TGMLibVclMap]);
+  AddBitmapToSplashScreen;
 end;
 
 end.
